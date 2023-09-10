61844e4051d5d8660c351d799e649cc880b18b06
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2023-09-10 17:26:40 +0300

    some fixes

--- Client/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-// import { SignUptype } from "../Components/ui/formtype";
-// import { SubmitHandler} from 'react-hook-form';
-
-
-// export const SubmitHandle: SubmitHandler<Formtype> = (data) => {
-      
-//   console.log(data);
-// }
