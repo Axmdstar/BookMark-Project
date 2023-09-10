02adda83cd23b5f0c79f986823c9d725c9156ed6
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2023-09-10 16:56:16 +0300

    fix some warning

--- Client/src/Components/MainBk.tsx
+++ Client/src/Components/MainBk.tsx
@@ -12,11 +12,11 @@ import { LiaUserAltSolid } from "react-icons/lia";
 // but since it's small keep it in Mainbk
 
 const MainBk = () => {
-  const {loggingOut, userdata} = useFetch();
+  const { loggingOut, userdata } = useFetch();
   const [ddowntoggle, setDdowntoggle] = useState(false);
   const [togglePage, setTogglePage] = useState(true);
 
-  const toExplore = () =>{
+  const toExplore = () => {
     setTogglePage(false);
   }
 
@@ -24,36 +24,36 @@ const MainBk = () => {
     setTogglePage(true);
   }
 
-  
-    return (   
-      <section className="pt-[56px] ">
-        {/* NavBar  */}
-        <nav className='flex fixed  w-full z-50 top-0 bg-[#6650df] h-[56px]  items-center '>
-          <div className="w-full top-1 bg-violet-600 shadow-md blur fixed h-[56px] -z-50"></div>
-          <h2 className="font-customtt font-extrabold text-2xl text-violet-50 p-5">BookMark.</h2>
-          <h2 onClick={toExplore} className="text-white cursor-pointer ">Explore</h2>
-          <button className=" ml-auto mr-4 relative" onClick={() => { setDdowntoggle(!ddowntoggle) }}>
-            <div className="flex  p-2">
-            <LiaUserAltSolid className="text-OvsP-o500 text-2xl "/>
+
+  return (
+    <section className="pt-[56px] ">
+      {/* NavBar  */}
+      <nav className='flex fixed  w-full z-50 top-0 bg-[#6650df] h-[56px]  items-center '>
+        <div className="w-full top-1 bg-violet-600 shadow-md blur fixed h-[56px] -z-50"></div>
+        <h2 className="font-customtt font-extrabold text-2xl text-violet-50 p-5">BookMark.</h2>
+        <h2 onClick={toExplore} className="text-white cursor-pointer ">Explore</h2>
+        <div className=" ml-auto mr-4 relative" onClick={() => { setDdowntoggle(!ddowntoggle) }}>
+          <div className="flex  p-2">
+            <LiaUserAltSolid className="text-OvsP-o500 text-2xl " />
             <p className="px-3 py-0 text-white font-semibold hover:text-orange-400">{userdata.name} </p>
-            </div>
-            
+          </div>
 
-            <div className={`flex-col w-40 right-0 z-50 absolute ${ddowntoggle ? "flex" : "hidden"}`}>
-              <button className="pr-5 pl-1 py-2 mx-3 bg-violet-100 text-left text-violet-900 hover:bg-violet-400 hover:text-white" onClick={toHome}>My Bookmarks </button>
-              <button className="pr-5 pl-1 py-2 mx-3 bg-violet-100 text-left text-violet-900 hover:bg-violet-400 hover:text-white" onClick={loggingOut}>Logout</button>
-            </div>
 
-          </button>
-        </nav>
+          <div className={`flex-col w-40 right-0 z-50 absolute ${ddowntoggle ? "flex" : "hidden"}`}>
+            <button className="pr-5 pl-1 py-2 mx-3 bg-violet-100 text-left text-violet-900 hover:bg-violet-400 hover:text-white" onClick={toHome}>My Bookmarks </button>
+            <button className="pr-5 pl-1 py-2 mx-3 bg-violet-100 text-left text-violet-900 hover:bg-violet-400 hover:text-white" onClick={loggingOut}>Logout</button>
+          </div>
 
-        {/* switch btw Userbkmk, Explore */}
-        <main className="w-[96%] mr-auto ml-auto ">
-          {togglePage ? <Userbkmk /> : <Explore />}
-        </main >
-        
-      </section>
-    );
+        </div>
+      </nav>
+
+      {/* switch btw Userbkmk, Explore */}
+      <main className="w-[96%] mr-auto ml-auto ">
+        {togglePage ? <Userbkmk /> : <Explore />}
+      </main >
+
+    </section>
+  );
 }
- 
+
 export default MainBk;
--- Client/src/Components/ui/bkcard.tsx
+++ Client/src/Components/ui/bkcard.tsx
@@ -6,10 +6,10 @@ import useFetch from '../Hooks/UseFetch';
 // GetBkid() Gets the bookmark id 
 
 interface bkarrobj {
-    bookmarkid : string,
-    title : string,
-    likes:number,
-    name?:string
+    bookmarkid: string,
+    title: string,
+    likes: number,
+    name?: string
 }
 
 interface BkmkProps {
@@ -26,10 +26,10 @@ const Bkcard = ({ bkarr, GetBkid } :BkmkProps ) => {
         <div className="md:grid grid-cols-4 gap-3 ">
 
         {
-        bkarr?.map((bk:bkarrobj ) =>
+        bkarr?.map((bk:bkarrobj, i ) =>
         {
             return(
-                <div className="py-2">
+                <div className="py-2" key={i}>
                     <div key={bk.bookmarkid} className="p-5 flex w-full duration-75 rounded-md shadow-md shadow-violet-400 bg-gradient-to-l from-10% to-violet-50 from-violet-100 hover:to-orange-50 hover:from-orange-100 hover:shadow-orange-400">
                         <button id={bk.bookmarkid} onClick={GetBkid} className='w-full' >
                             <div className='text-left'>
