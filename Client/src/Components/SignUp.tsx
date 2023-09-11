5d746e52023143263a1d1ba70b0371a13efc7441
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2023-09-11 18:20:54 +0300

    added alert in sigin component

--- Client/src/Components/SignUp.tsx
+++ Client/src/Components/SignUp.tsx
@@ -43,6 +43,7 @@ export const SignUp = ({ setSigning }: { setSigning: React.Dispatch<React.SetSta
     }
 
 
+    alert("Use a Fake email\n\nFake email istacmal");
     const buttonstyle = "mb-2 block w-full rounded bg-violet-500 px-6 pb-2 pt-2.5 text-xl font-medium uppercase hover:bg-OvsP-p400 leading-normal text-white shadow-violet-800 shadow-lg transition duration-150 ease-in-out  hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-OvsP-p200 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-OvsP-p500 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] "
 
     return (
